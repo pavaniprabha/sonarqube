0be217e66e5e6c9908a1dabd6c52f363e4ceb779
pavaniprabha <88011008+pavaniprabha@users.noreply.github.com>
2022-03-29 15:11:30 +0530

    Delete code.java

--- code.java
+++
@@ -1,9 +0,0 @@
-
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        a = 3 + 3;
-        System.out.println("Hello, World!"); 
-        System.out.println(a);
-    }
-}
